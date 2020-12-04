b2026d71df7dc841a917a72f7ef4536286b57fa1
Francesco Di Mauro <frdim@microsoft.com>
2020-12-04 08:25:23 +0100

    Day 2 was easy

--- Day2/solution2.ts
+++ Day2/solution2.ts
@@ -6,7 +6,7 @@ const input = inputData.problem;
 let valid = 0;
 
 input.forEach(line => {
-  const [_full, min, max, char, pwd] = line.match(lineRegex);
+  const [full, min, max, char, pwd] = line.match(lineRegex);
 
   const isValid = validatePassword(min, max, char, pwd);
 
@@ -16,13 +16,14 @@ input.forEach(line => {
 console.log("Found", valid, "valid passwords");
 
 function validatePassword(
-  min: string,
-  max: string,
+  pos1: string,
+  pos2: string,
   char: string,
   pwd: string
 ): boolean {
-  const count = [...pwd].filter(letter => letter === char).length
+  const chr1 = pwd.charAt(parseInt(pos1) - 1);
+  const chr2 = pwd.charAt(parseInt(pos2) - 1);
 
-  return count >= parseInt(min) && count <= parseInt(max);
+  return chr1 === char ? chr2 !== char : chr2 === char;
 }
 

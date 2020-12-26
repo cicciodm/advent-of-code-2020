be90d46936b7f2335140ade38191bcf2a68bc8b0
Francesco Di Mauro <frdim@microsoft.com>
2020-12-26 17:08:10 +0100

    cleanup

--- Day10/solution2.ts
+++ Day10/solution2.ts
@@ -6,10 +6,6 @@ let sortedJoltages = data.sort((a, b) => a - b);
 const maxJoltage = sortedJoltages[sortedJoltages.length - 1] + 3;
 sortedJoltages = [0, ...sortedJoltages, maxJoltage];
 
-console.log("Here's the available joltages, sorted", sortedJoltages);
-
-const joltagesSet = new Set(sortedJoltages);
-
 const joltagesPossibilities = {};
 
 const joltagesToLast = findPossibilitiesFor(maxJoltage, sortedJoltages);
@@ -29,7 +25,6 @@ function findPossibilitiesFor(
   }
 
   const previousAdapters = joltageList.filter(jolt => joltage - jolt > 0 && joltage - jolt <= 3);
-  console.log("Will look for paths to", previousAdapters);
   const previousPossibilities = previousAdapters.map(jolt => findPossibilitiesFor(jolt, joltageList));
   const sumOfPoss = previousPossibilities.reduce((total, poss) => poss + total, 0);
 
